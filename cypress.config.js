4e7a6d034879b03510a7591ce6553d8292bd0c01
Leen-odeh3 <leenodeh287@gmail.com>
2025-06-15 00:39:59 +0300

    add videos and report

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,16 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
     },
-    baseUrl:"https://burger-builder.habibhinn.com/"
+    baseUrl:"https://burger-builder.habibhinn.com/",
+    reporter: "mochawesome",
+      reporterOptions: {
+        reportDir: "cypress/myReport", 
+        overwrite: false,
+        html: true,
+        json: false,
+        timestamp: "mmddyyyy_HHMMss"
+    },
+    video:true,
+    videoCompression:0
   },
 });
