f39c57397c68282661ddb5d4445ddd0e20bb85ff
Leen-odeh3 <leenodeh287@gmail.com>
2025-06-22 08:01:55 +0300

    tc04-api : register api testing

---
+++ cypress/e2e/apiTesting/signup_api_test.cy.js
@@ -0,0 +1,23 @@
+describe('Firebase Sign Up API Test', () => {
+  it('should create a new user successfully', () => {
+    cy.request({
+      method: 'POST',
+      url: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBQ1kZviUNMJNYG_PezyUSawxN1JcHOD28',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: {
+        email: `testuser_${Date.now()}@example.com`,
+        password: '12345678',
+        returnSecureToken: true,
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('idToken');
+      expect(response.body).to.have.property('email');
+      expect(response.body).to.have.property('refreshToken');
+      expect(response.body).to.have.property('expiresIn');
+      expect(response.body).to.have.property('localId');
+    });
+  });
+});
